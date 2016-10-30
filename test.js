84f8e989453c38743f426f30216bee7c893601b6
Chris Greene <archgoon@gmail.com>
2016-10-30 16:50:04 -0700

    Rotating box, with lights, now has proper card dimensions.

--- test.js
+++ test.js
@@ -1,21 +1,31 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+"use strict";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+const positions = [[0,0,-10],[-10,0,0],[0,0,10],[10,10,0]]
+for (const position of positions) {
+  const light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( ...position ).normalize();
+  scene.add(light);
+}
+
+const geometry = new THREE.BoxGeometry( 2.5, 3.5, .0115 );
+const material = new THREE.MeshPhongMaterial( {
+  ambient: 0x050505, color: 0x555555,
+  specular: 0x555555, shininess: 1 } );
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 
 function render() {
 	requestAnimationFrame( render );
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 render();
