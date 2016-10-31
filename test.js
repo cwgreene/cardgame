1d43d43d666ad8712111ddc812d0355604bdda8e
Chris Greene <archgoon@gmail.com>
2016-10-30 17:02:33 -0700

    Switching to typescript.

--- test.js
+++ test.js
@@ -1,31 +1,26 @@
 "use strict";
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const positions = [[0,0,-10],[-10,0,0],[0,0,10],[10,10,0]]
-for (const position of positions) {
-  const light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( ...position ).normalize();
-  scene.add(light);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+var positions = [[0, 0, -10], [-10, 0, 0], [0, 0, 10], [10, 10, 0]];
+for (var _i = 0, positions_1 = positions; _i < positions_1.length; _i++) {
+    var position = positions_1[_i];
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(position[0], position[1], position[2]).normalize();
+    scene.add(light);
 }
-
-const geometry = new THREE.BoxGeometry( 2.5, 3.5, .0115 );
-const material = new THREE.MeshPhongMaterial( {
-  ambient: 0x050505, color: 0x555555,
-  specular: 0x555555, shininess: 1 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+var geometry = new THREE.BoxGeometry(2.5, 3.5, .0115);
+var material = new THREE.MeshPhongMaterial({ color: 0x555555,
+    specular: 0x555555, shininess: 1 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 camera.position.z = 5;
-
 function render() {
-	requestAnimationFrame( render );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    requestAnimationFrame(render);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
 render();
---
+++ test.ts
@@ -0,0 +1,30 @@
+"use strict";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const positions = [[0,0,-10],[-10,0,0],[0,0,10],[10,10,0]]
+for (const position of positions) {
+  const light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( position[0], position[1], position[2] ).normalize();
+  scene.add(light);
+}
+
+const geometry = new THREE.BoxGeometry( 2.5, 3.5, .0115 );
+const material = new THREE.MeshPhongMaterial( { color: 0x555555,
+  specular: 0x555555, shininess: 1 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function render() {
+	requestAnimationFrame( render );
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+render();
